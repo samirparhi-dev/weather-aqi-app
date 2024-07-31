dc91b88a0f6f58069a3413192dfb9814e90aa172
samirparhi-dev <samirparhi@gmail.com>
2024-07-31 16:49:09 +0530

    The First cut
    Signed-off-by: samirparhi-dev <samirparhi@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import WeatherAQI from './WeatherAQI';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header Text-purple flex flex-wrap">
         <WeatherAQI />
       </header>
     </div>
